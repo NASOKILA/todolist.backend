35f78b6cb7589d03d871becf96345d4de61947f3
Atanas <atanasskambitovv@gmail.com>
2020-01-29 02:45:38 +0200

    .

--- TodoList.Backend/TodoList.Backend/Controllers/ValuesController.cs
+++ TodoList.Backend/TodoList.Backend/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Totolist.Backend.Controllers
+namespace TodoList.Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- TodoList.Backend/TodoList.Backend/Program.cs
+++ TodoList.Backend/TodoList.Backend/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Totolist.Backend
+namespace TodoList.Backend
 {
     public class Program
     {
